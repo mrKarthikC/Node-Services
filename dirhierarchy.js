67d78bf931383e0cbef94ba05581cf48b449de35
Karthik Chilukuri <37879494+mrKarthikC@users.noreply.github.com>
2018-05-13 19:26:15 +0530

    node app to list directory hierarchy

---
+++ dirhierarchy.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+function printDirHeirarchy(dir) {
+    console.log(dir);
+    let files = fs.readdirSync(dir);
+    let filePath;
+    for (let file of files) {
+        filePath = dir + '/' + file;
+        let stat = fs.statSync(filePath);
+        if (stat.isFile()) {
+            console.log(filePath);
+        }
+        else {
+            printDirHeirarchy(filePath);
+        }
+    }
+}
+printDirHeirarchy(__dirname);
